2e300211863b87641bcb7a1c7d9ba4aaf7c1cfe7
Kerveck <kerveck.vornak@yandex.ru>
2016-06-15 11:48:06 +0300

    add google storage

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -21,7 +21,7 @@ class MicropostsController < ApplicationController
     #render plain: params[:micropost].inspect
     @micropost = current_user.microposts.build(micropost_params) # unless micropost_params.blank?
     if @micropost.save # && !@micropost.content.empty?
-      @image = @micropost.images.create!(:file => @micropost.image.file, :micropost_id => @micropost.id)
+      @image = @micropost.images.create!(:file => @micropost.images, :micropost_id => @micropost.id)
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
@@ -38,7 +38,8 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content, :tag_list, images_attributes: [:id, :micropost_id, :file]) # :file, :name,
+      params.require(:micropost).permit(:content, :tag_list, images_attributes: [:id, :micropost_id, :file])
+      #params.require(:micropost).permit(:content, :name, :tag_list, :image) # [:id, :micropost_id, :file]:file, :name,
     end
 
     def correct_user
